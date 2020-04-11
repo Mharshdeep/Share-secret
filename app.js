5510ba6d7d2dfd4ff419751aefe3d1e1202f6d37
harshdeep singh <meeku102@gmail.com>
2020-04-11 11:58:29 +0530

    modified listen function

--- app.js
+++ app.js
@@ -207,6 +207,10 @@ if(err){
   });
   });
 
-app.listen(3000, function() {
-console.log("server has started on port 3000");
+let port = process.env.PORT;
+if (port="" || or port = null){
+  port = 3000;
+}
+app.listen(process.env.PORT, function() {
+console.log("server has started Successfully");
 })
