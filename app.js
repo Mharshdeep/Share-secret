ca9deb52f614092736fbc63316e2eccadac30afa
harshdeep singh <meeku102@gmail.com>
2020-04-11 12:40:07 +0530

    modified listen function again_4

--- app.js
+++ app.js
@@ -207,10 +207,8 @@ if(err){
   });
   });
 
-let port = process.env.PORT;
-if (port== "" ||  port == null){
-  port = 3000;
-}
+const port = process.env.PORT || 3000;
+
 app.listen(port, function() {
 console.log("server has started Successfully");
 })
